555a56fdc42250d1123e921439cd6ee97b7c34f8
Charles Christian <charles.pilapil@philcentro.ph>
2024-07-13 01:51:29 +0800

    bug fix

--- src/app/discover/[projectId]/page.tsx
+++ src/app/discover/[projectId]/page.tsx
@@ -1,83 +1,87 @@
-"use client";
+'use client';
 
-import { CardLayout } from "@/app/ui/discover/CardLayout";
-import { ProjectDetailCard } from "@/app/ui/discover/ProjectDetailCard";
-import { data } from "../page";
-import { usePathname } from "next/navigation";
-import { PieChartDonutComponent } from "@/app/ui/charts/pie-chart-donut";
-import Link from "next/link";
+import { CardLayout } from '@/app/ui/discover/CardLayout';
+import { ProjectDetailCard } from '@/app/ui/discover/ProjectDetailCard';
+import { data } from '../page';
+import { usePathname } from 'next/navigation';
+import { PieChartDonutComponent } from '@/app/ui/charts/pie-chart-donut';
+import Link from 'next/link';
 
-const page = () => {
+const Page = () => {
   const pathname = usePathname();
-  const contractId = pathname.split("/discover/")[1];
+  const contractId = pathname.split('/discover/')[1];
   const projectDetails = data.filter(
     (data) => contractId === data.contractId
   )[0];
 
   return (
-    <div className="flex justify-center p-14">
-      <div className="w-[60%]">
-        <Link href={"/discover"}>
-          <p className="text-blue-500 mb-6">&lt; Project List</p>
+    <div className='flex justify-center p-14'>
+      <div className='w-[60%]'>
+        <Link href={'/discover'}>
+          <p className='text-blue-500 mb-6'>&lt; Project List</p>
         </Link>
-        <div className="flex justify-between mb-[24px]">
+        <div className='flex justify-between mb-[24px]'>
           <div>
-            <p className="inline-block font-bold bg-[#0E4370] px-[10px] py-[5px] rounded-sm text-white tracking-widest mb-6">
+            <p className='inline-block font-bold bg-[#0E4370] px-[10px] py-[5px] rounded-sm text-white tracking-widest mb-6'>
               ProjectID
             </p>
-            <div className="flex items-center mb-4">
-              <p className="font-bold">Date Started : </p>
-              <p className="px-[10px] py-[5px] ml-[10px] text-white text-xs bg-[#00e272] rounded-lg">
+            <div className='flex items-center mb-4'>
+              <p className='font-bold'>Date Started : </p>
+              <p className='px-[10px] py-[5px] ml-[10px] text-white text-xs bg-[#00e272] rounded-lg'>
                 {projectDetails.dateStarted}
               </p>
             </div>
-            <div className="flex items-center mb-4">
-              <p className="font-bold">Date of Completion : </p>
-              <p className="px-[10px] py-[5px] ml-[10px] text-white text-xs bg-[#E84A5A] rounded-lg">
+            <div className='flex items-center mb-4'>
+              <p className='font-bold'>Date of Completion : </p>
+              <p className='px-[10px] py-[5px] ml-[10px] text-white text-xs bg-[#E84A5A] rounded-lg'>
                 {projectDetails.dateCompletion}
               </p>
             </div>
           </div>
-          <div className="w-[45%] h-[30%]">
+          <div className='w-[45%] h-[30%]'>
             <PieChartDonutComponent />
           </div>
         </div>
         <CardLayout
-          title={"Project Details"}
-          style="p-[24px] mb-6"
-          headerColor="#0E4370"
+          title={'Project Details'}
+          style='p-[24px] mb-6'
+          headerColor='#0E4370'
         >
           {projectDetails?.details?.map((item) => (
-            <ProjectDetailCard leftData={item[0]} rightData={item[1]} />
+            <ProjectDetailCard
+              key={item[0].name}
+              leftData={item[0]}
+              rightData={item[1]}
+            />
           ))}
         </CardLayout>
         <CardLayout
-          title={"Project Team Members"}
-          style="p-[24px]"
-          headerColor="#0E4370"
+          title={'Project Team Members'}
+          style='p-[24px]'
+          headerColor='#0E4370'
         >
-          <div className="grid grid-cols-2 mb-4 border-2 rounded-b-lg border-gray-500">
+          <div className='grid grid-cols-2 mb-4 border-2 rounded-b-lg border-gray-500'>
             <div>
               <p
                 className={`h-[30px] w-[100%] bg-[#0c5acb] rounded-tl-[8px] px-[10px] flex items-center text-white`}
               >
-                {"Name of Member"}
+                {'Name of Member'}
               </p>
-              <p className="px-[10px] py-[5px]">{"John De Leon"}</p>
-              <p className="px-[10px] py-[5px]">{"Jane Dela Cruz"}</p>
-              <p className="px-[10px] py-[5px]">{"Charles Santos"}</p>
-              <p className="px-[10px] py-[5px]">{"Emily Carranza"}</p>
+              <p className='px-[10px] py-[5px]'>{'John De Leon'}</p>
+              <p className='px-[10px] py-[5px]'>{'Jane Dela Cruz'}</p>
+              <p className='px-[10px] py-[5px]'>{'Charles Santos'}</p>
+              <p className='px-[10px] py-[5px]'>{'Emily Carranza'}</p>
             </div>
             <div>
               <p
                 className={`h-[30px] w-[100%] bg-[#0c5acb] rounded-tr-[8px] px-[10px] flex items-center text-white`}
               >
-                {"Role"}
+                {'Role'}
               </p>
-              <p className="px-[10px] py-[5px]">{"Project Engineer"}</p>
-              <p className="px-[10px] py-[5px]">{"Materials Engineer"}</p>
-              <p className="px-[10px] py-[5px]">{"Site Engineer"}</p>
-              <p className="px-[10px] py-[5px]">{"Project Coordinator"}</p>
+              <p className='px-[10px] py-[5px]'>{'Project Engineer'}</p>
+              <p className='px-[10px] py-[5px]'>{'Materials Engineer'}</p>
+              <p className='px-[10px] py-[5px]'>{'Site Engineer'}</p>
+              <p className='px-[10px] py-[5px]'>{'Project Coordinator'}</p>
             </div>
           </div>
         </CardLayout>
@@ -86,4 +90,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
--- src/app/discover/page.tsx
+++ src/app/discover/page.tsx
@@ -1,270 +1,270 @@
-import Filter from "../ui/discover/filter";
-import Map from "../ui/discover/map";
-import DataSection from "../ui/discover/data-section";
-import Charts from "../ui/discover/Charts";
-import { ProjectCard } from "../ui/discover/ProjectCard";
-import { CardLayout } from "../ui/discover/CardLayout";
-import Link from "next/link";
+import Filter from '../ui/discover/filter';
+import Map from '../ui/discover/map';
+import DataSection from '../ui/discover/data-section';
+import Charts from '../ui/discover/Charts';
+import { ProjectCard } from '../ui/discover/ProjectCard';
+import { CardLayout } from '../ui/discover/CardLayout';
+import Link from 'next/link';
 
 export const data = [
   {
-    contractId: "1",
-    dateStarted: "June 7, 2024",
-    dateCompletion: "October 9, 2024",
-    contractor: "A Construction",
-    accomplishmentRate: "52.64",
+    contractId: '1',
+    dateStarted: 'June 7, 2024',
+    dateCompletion: 'October 9, 2024',
+    contractor: 'A Construction',
+    accomplishmentRate: '52.64',
     details: [
       [
         {
-          name: "Contract ID",
-          data: "21IDLN155",
+          name: 'Contract ID',
+          data: '21IDLN155',
         },
         {
-          name: "Contract Name",
-          data: "Construction of Libonao River",
-        },
-      ],
-      [
-        {
-          name: "Contractor",
-          data: "A Construction",
-        },
-        {
-          name: "Implementing Office Name",
-          data: "Construction of Libonao River",
+          name: 'Contract Name',
+          data: 'Construction of Libonao River',
         },
       ],
       [
         {
-          name: "Duration",
-          data: "21IDLN155",
+          name: 'Contractor',
+          data: 'A Construction',
         },
         {
-          name: "Fiscal Year",
-          data: "Construction of Libonao River",
-        },
-      ],
-      [
-        {
-          name: "Project Category",
-          data: "21IDLN155",
-        },
-        {
-          name: "Source of Fund",
-          data: "Construction of Libonao River",
+          name: 'Implementing Office Name',
+          data: 'Construction of Libonao River',
         },
       ],
       [
         {
-          name: "Mobilization Fee",
-          data: "21IDLN155",
+          name: 'Duration',
+          data: '21IDLN155',
         },
         {
-          name: "Date of Award",
-          data: "Construction of Libonao River",
+          name: 'Fiscal Year',
+          data: 'Construction of Libonao River',
+        },
+      ],
+      [
+        {
+          name: 'Project Category',
+          data: '21IDLN155',
+        },
+        {
+          name: 'Source of Fund',
+          data: 'Construction of Libonao River',
+        },
+      ],
+      [
+        {
+          name: 'Mobilization Fee',
+          data: '21IDLN155',
+        },
+        {
+          name: 'Date of Award',
+          data: 'Construction of Libonao River',
         },
       ],
     ],
   },
   {
-    contractId: "2",
-    dateStarted: "August 14, 2024",
-    dateCompletion: "October 5, 2024",
-    contractor: "B Construction",
-    accomplishmentRate: "60.64",
+    contractId: '2',
+    dateStarted: 'August 14, 2024',
+    dateCompletion: 'October 5, 2024',
+    contractor: 'B Construction',
+    accomplishmentRate: '60.64',
     details: [
       [
         {
-          name: "Contract ID",
-          data: "21IDLN155",
+          name: 'Contract ID',
+          data: '21IDLN155',
         },
         {
-          name: "Contract Name",
-          data: "Construction of Libonao River",
-        },
-      ],
-      [
-        {
-          name: "Contractor",
-          data: "A Construction",
-        },
-        {
-          name: "Implementing Office Name",
-          data: "Construction of Libonao River",
+          name: 'Contract Name',
+          data: 'Construction of Libonao River',
         },
       ],
       [
         {
-          name: "Duration",
-          data: "21IDLN155",
+          name: 'Contractor',
+          data: 'A Construction',
         },
         {
-          name: "Fiscal Year",
-          data: "Construction of Libonao River",
-        },
-      ],
-      [
-        {
-          name: "Project Category",
-          data: "21IDLN155",
-        },
-        {
-          name: "Source of Fund",
-          data: "Construction of Libonao River",
+          name: 'Implementing Office Name',
+          data: 'Construction of Libonao River',
         },
       ],
       [
         {
-          name: "Mobilization Fee",
-          data: "21IDLN155",
+          name: 'Duration',
+          data: '21IDLN155',
         },
         {
-          name: "Date of Award",
-          data: "Construction of Libonao River",
+          name: 'Fiscal Year',
+          data: 'Construction of Libonao River',
+        },
+      ],
+      [
+        {
+          name: 'Project Category',
+          data: '21IDLN155',
+        },
+        {
+          name: 'Source of Fund',
+          data: 'Construction of Libonao River',
+        },
+      ],
+      [
+        {
+          name: 'Mobilization Fee',
+          data: '21IDLN155',
+        },
+        {
+          name: 'Date of Award',
+          data: 'Construction of Libonao River',
         },
       ],
     ],
   },
   {
-    contractId: "3",
-    dateStarted: "August 14, 2024",
-    dateCompletion: "October 5, 2024",
-    contractor: "B Construction",
-    accomplishmentRate: "60.64",
+    contractId: '3',
+    dateStarted: 'August 14, 2024',
+    dateCompletion: 'October 5, 2024',
+    contractor: 'B Construction',
+    accomplishmentRate: '60.64',
     details: [
       [
         {
-          name: "Contract ID",
-          data: "21IDLN155",
+          name: 'Contract ID',
+          data: '21IDLN155',
         },
         {
-          name: "Contract Name",
-          data: "Construction of Libonao River",
-        },
-      ],
-      [
-        {
-          name: "Contractor",
-          data: "A Construction",
-        },
-        {
-          name: "Implementing Office Name",
-          data: "Construction of Libonao River",
+          name: 'Contract Name',
+          data: 'Construction of Libonao River',
         },
       ],
       [
         {
-          name: "Duration",
-          data: "21IDLN155",
+          name: 'Contractor',
+          data: 'A Construction',
         },
         {
-          name: "Fiscal Year",
-          data: "Construction of Libonao River",
-        },
-      ],
-      [
-        {
-          name: "Project Category",
-          data: "21IDLN155",
-        },
-        {
-          name: "Source of Fund",
-          data: "Construction of Libonao River",
+          name: 'Implementing Office Name',
+          data: 'Construction of Libonao River',
         },
       ],
       [
         {
-          name: "Mobilization Fee",
-          data: "21IDLN155",
+          name: 'Duration',
+          data: '21IDLN155',
         },
         {
-          name: "Date of Award",
-          data: "Construction of Libonao River",
+          name: 'Fiscal Year',
+          data: 'Construction of Libonao River',
+        },
+      ],
+      [
+        {
+          name: 'Project Category',
+          data: '21IDLN155',
+        },
+        {
+          name: 'Source of Fund',
+          data: 'Construction of Libonao River',
+        },
+      ],
+      [
+        {
+          name: 'Mobilization Fee',
+          data: '21IDLN155',
+        },
+        {
+          name: 'Date of Award',
+          data: 'Construction of Libonao River',
         },
       ],
     ],
   },
   {
-    contractId: "4",
-    dateStarted: "August 14, 2024",
-    dateCompletion: "October 5, 2024",
-    contractor: "B Construction",
-    accomplishmentRate: "60.64",
+    contractId: '4',
+    dateStarted: 'August 14, 2024',
+    dateCompletion: 'October 5, 2024',
+    contractor: 'B Construction',
+    accomplishmentRate: '60.64',
     details: [
       [
         {
-          name: "Contract ID",
-          data: "21IDLN155",
+          name: 'Contract ID',
+          data: '21IDLN155',
         },
         {
-          name: "Contract Name",
-          data: "Construction of Libonao River",
-        },
-      ],
-      [
-        {
-          name: "Contractor",
-          data: "A Construction",
-        },
-        {
-          name: "Implementing Office Name",
-          data: "Construction of Libonao River",
+          name: 'Contract Name',
+          data: 'Construction of Libonao River',
         },
       ],
       [
         {
-          name: "Duration",
-          data: "21IDLN155",
+          name: 'Contractor',
+          data: 'A Construction',
         },
         {
-          name: "Fiscal Year",
-          data: "Construction of Libonao River",
-        },
-      ],
-      [
-        {
-          name: "Project Category",
-          data: "21IDLN155",
-        },
-        {
-          name: "Source of Fund",
-          data: "Construction of Libonao River",
+          name: 'Implementing Office Name',
+          data: 'Construction of Libonao River',
         },
       ],
       [
         {
-          name: "Mobilization Fee",
-          data: "21IDLN155",
+          name: 'Duration',
+          data: '21IDLN155',
         },
         {
-          name: "Date of Award",
-          data: "Construction of Libonao River",
+          name: 'Fiscal Year',
+          data: 'Construction of Libonao River',
+        },
+      ],
+      [
+        {
+          name: 'Project Category',
+          data: '21IDLN155',
+        },
+        {
+          name: 'Source of Fund',
+          data: 'Construction of Libonao River',
+        },
+      ],
+      [
+        {
+          name: 'Mobilization Fee',
+          data: '21IDLN155',
+        },
+        {
+          name: 'Date of Award',
+          data: 'Construction of Libonao River',
         },
       ],
     ],
   },
 ];
 
-const page = () => {
+const Page = () => {
   return (
-    <main className="container mt-[96px]">
+    <main className='container mt-[96px]'>
       <Filter />
       <DataSection />
       <Map />
       <Charts />
       <CardLayout
-        title={"Project List"}
-        style="mb-[16px]"
-        headerColor="#0E4370"
+        title={'Project List'}
+        style='mb-[16px]'
+        headerColor='#0E4370'
       >
-        <div className="border border-[#ccc] rounded-b-[8px] px-6">
-          <div className="flex justify-between">
-            <p className="px-4 py-2 border-b border-gray-400 font-bold">2024</p>
-            <p className="px-4 py-2 border-b border-gray-400 font-bold">
+        <div className='border border-[#ccc] rounded-b-[8px] px-6'>
+          <div className='flex justify-between'>
+            <p className='px-4 py-2 border-b border-gray-400 font-bold'>2024</p>
+            <p className='px-4 py-2 border-b border-gray-400 font-bold'>
               Region I / Metro Manila / Makati
             </p>
           </div>
-          <div className="p-6 max-h-[500px] overflow-y-auto">
+          <div className='p-6 max-h-[500px] overflow-y-auto'>
             {data.map((data) => {
               return (
                 <Link
@@ -287,4 +287,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
