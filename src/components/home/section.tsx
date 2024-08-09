d5e5a683cd515ba291b92ae6daaa5dc8743f5b53
RaphaelSalayog006 <raphael.salayog@philcentro.ph>
2024-08-09 12:58:03 +0800

    Fix: Section Component

--- src/components/home/section.tsx
+++ src/components/home/section.tsx
@@ -5,14 +5,7 @@ interface ReactNodeProps {
 }
 
 const Section = ({ children }: ReactNodeProps) => {
-    return (
-        <section
-            className='mb-20 container'
-            style={{ minHeight: 'calc(100vh - 100px)' }}
-        >
-            {children}
-        </section>
-    );
+    return <section className='mb-20 container'>{children}</section>;
 };
 
 export default Section;
