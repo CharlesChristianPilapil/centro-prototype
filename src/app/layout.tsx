720d39f1ce7389e4fcd26548e31b81253a6c0009
RaphaelSalayog006 <raphael.salayog@philcentro.ph>
2024-08-12 11:06:36 +0800

    Feat: Add Progress Bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,36 @@
-import type { Metadata } from 'next';
+'use client';
+
 import { Poppins } from 'next/font/google';
 import './globals.css';
 import Navbar from '@/components/NavBar';
 import Footer from '@/components/Footer';
 import Newsletter from '@/components/Newsletter';
+import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
 
 const poppins = Poppins({
-  subsets: ['latin'],
-  weight: '400',
+    subsets: ['latin'],
+    weight: '400',
 });
 
-export const metadata: Metadata = {
-  title: 'Philcentro Technologies Inc.',
-  description: 'Philcentro Technologies Inc.',
-  icons: {
-    icon: './next.svg'
-  }
-};
-
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang='en'>
-      <body className={`${poppins.className} bg-base`}>
-        <Navbar />
-        {children}
-        <Newsletter />
-        <Footer />
-      </body>
-    </html>
-  );
+    return (
+        <html lang='en'>
+            <body className={`${poppins.className} bg-base`}>
+                <ProgressBar
+                    height='6px'
+                    color='#2563eb'
+                    options={{ showSpinner: false }}
+                    shallowRouting
+                />
+                <Navbar />
+                {children}
+                <Newsletter />
+                <Footer />
+            </body>
+        </html>
+    );
 }
--- src/app/loading.tsx
+++
@@ -1,8 +0,0 @@
-const LoadingPage = () => {
-  return (
-    <div className="grid place-items-center h-[100vh]">
-      <h1 className="text-7xl"> Page Loading... </h1>
-    </div>
-  )
-}
-export default LoadingPage
